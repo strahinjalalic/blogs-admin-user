93d91b220657e48e6a507bf46478c191633e4daf
Strahinja <strahinjalalic10@gmail.com>
2019-09-20 13:50:36 +0200

    added admin layouts and controller

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,9 +13,11 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
+            $table->increments('id')->index()->unsigned()->nullable();
+            $table->integer('role_id');
+            $table->integer('is_active')->default(0);
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
